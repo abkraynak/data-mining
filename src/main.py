6e6a3d6969042c8f1d36d57f44e4edde98c75bb7
SKalltheway <samkaczynski@gmail.com>
2021-10-21 15:22:39 -0400

    added comments about what insights we are looking for

--- src/main.py
+++ src/main.py
@@ -6,9 +6,20 @@ import matplotlib.pyplot as plt
 DATA_PATH = 'data/survey_results_public.csv'
 #DATA_PATH = 'data/small_sample.csv'
 
+#data pre-processing
+#read all information in and copy values we like into a new csv file 
+#disregard students, people living in the United States
+
+#Insights to find
+#-correlation between language and salary
+#-with years of experience and languages known, predict salary
+#-level of degree correlation with salary
+#-age correlation with languages known
+#-age 1st coded correlation with salary and degree recieved
+
 df = pd.read_csv(DATA_PATH)
 print(df.head())
-#print(df.describe())
+print(df.describe())
 
 fig_1 = plt.figure(num = 1, figsize = (5, 5))
 salary = fig_1.add_subplot()
@@ -16,3 +27,4 @@ salary = fig_1.add_subplot()
 salary.hist(df.ConvertedCompYearly)
 
 plt.show()
+
