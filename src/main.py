465ad49757c5d82d25e048f79a7bc0195e1dbb10
abkraynak <andrewbkraynak@gmail.com>
2021-10-25 14:11:14 -0400

    Updated preprocessing

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import csv
 
-#FILE_PATH = 'data/survey_results_public.csv'
-FILE_PATH = 'data/small_sample.csv'
+FILE_PATH = 'data/survey_results_public.csv'
+#FILE_PATH = 'data/small_sample.csv'
 
 #data pre-processing
 #read all information in and copy values we like into a new csv file 
@@ -19,17 +19,6 @@ FILE_PATH = 'data/small_sample.csv'
 #-age 1st coded correlation with salary and degree recieved
 
 def preprocess():
-    df = pd.read_csv(FILE_PATH)
-    #print(df.head())
-    #print(df.describe())
-
-    fig_1 = plt.figure(num = 1, figsize = (5, 5))
-    salary = fig_1.add_subplot()
-
-    #salary.hist(df.ConvertedCompYearly)
-
-    #plt.show()
-
     # Rows from CSV file to skip
     skip = []
 
@@ -64,11 +53,23 @@ def preprocess():
         dr = csv.reader(csvfile)
         print('here')
         for row in dr:
-            if row[3] not in countries or row[2] != 'Employed full-time':
-                skip.append(row[0])
+            if row[3] not in countries or row[2] != 'Employed full-time' or row[47] == 'NA':
+                if row[0] != 'ResponseId':
+                    skip.append(int(row[0]))
 
         # Print 
         print(len(skip))
+
+        df = pd.read_csv(FILE_PATH, skiprows=skip)
+        print(df.head())
+        #print(df.describe())
+
+        fig_1 = plt.figure(num = 1, figsize = (5, 5))
+        salary = fig_1.add_subplot()
+
+        #salary.hist(df.ConvertedCompYearly)
+
+        #plt.show()
 
 if __name__ == '__main__':
     preprocess()
