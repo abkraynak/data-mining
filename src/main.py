32b217d93d752bd5b7a5a04b780b9545f55c9181
abkraynak <andrewbkraynak@gmail.com>
2021-10-25 13:53:45 -0400

    Added preprocessing, reading file with

--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import csv
 
-DATA_PATH = 'data/survey_results_public.csv'
-#DATA_PATH = 'data/small_sample.csv'
+#FILE_PATH = 'data/survey_results_public.csv'
+FILE_PATH = 'data/small_sample.csv'
 
 #data pre-processing
 #read all information in and copy values we like into a new csv file 
@@ -17,14 +18,56 @@ DATA_PATH = 'data/survey_results_public.csv'
 #-age correlation with languages known
 #-age 1st coded correlation with salary and degree recieved
 
-df = pd.read_csv(DATA_PATH)
-print(df.head())
-print(df.describe())
+def preprocess():
+    df = pd.read_csv(FILE_PATH)
+    #print(df.head())
+    #print(df.describe())
 
-fig_1 = plt.figure(num = 1, figsize = (5, 5))
-salary = fig_1.add_subplot()
+    fig_1 = plt.figure(num = 1, figsize = (5, 5))
+    salary = fig_1.add_subplot()
 
-salary.hist(df.ConvertedCompYearly)
+    #salary.hist(df.ConvertedCompYearly)
 
-plt.show()
+    #plt.show()
 
+    # Rows from CSV file to skip
+    skip = []
+
+    # Countries we want data from
+    countries = [
+        'United States of America', 
+        'United Kingdom of Great Britain and Northern Ireland',
+        'France',
+        'Germany',
+        'Switzerland',
+        'Canada',
+        'Portugal',
+        'Spain',
+        'Netherlands',
+        'Italy',
+        'Poland',
+        'Sweden',
+        'Belgium'
+        'Finland',
+        'Denmark',
+        'Austria',
+        'Bulgaria',
+        'Croatia',
+        'Estonia',
+        'Czech Republic',
+        'Hungary',
+        'Ireland',
+        'Luxembourg'
+    ]
+
+    with open(FILE_PATH, 'r') as csvfile:
+        dr = csv.reader(csvfile)
+        print('here')
+        for row in dr:
+            if row[3] not in countries or row[2] != 'Employed full-time':
+                skip.append(row[0])
+
+        print(len(skip))
+
+if __name__ == '__main__':
+    preprocess()
