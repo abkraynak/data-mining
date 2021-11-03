5fa68ef33348321daaa5becc54a4650a64356269
abkraynak <andrewbkraynak@gmail.com>
2021-11-03 14:28:41 -0400

    Read gender data

--- src/main.py
+++ src/main.py
@@ -20,13 +20,17 @@ FILE_PATH = 'data/survey_results_public.csv'
 #-with years of experience and languages known, predict salary
 #-level of degree correlation with salary
 #-age correlation with languages known
-#-age 1st coded correlation with salary and degree recieved
+# Determine age1stCode category using Naive Bayes using yearly salary, gender, operating system, and degree recieved
 
 age_dict = {'Younger than 5 years': 0, '5 - 10 years': 1, '11 - 17 years': 2, '18 - 24 years': 3, 
     '25 - 34 years': 4, '35 - 44 years': 5, '45 - 54 years': 6, '55 - 64 years': 7, 'Older than 64 years': 8}
-agefirstcoded = [[], [], [], [], [], [], [], [], []]
+salary_age1stcode = [[], [], [], [], [], [], [], [], []]
+gender_dict = {'Man': 0, 'Woman': 1, 'Non-binary, genderqueer, or gender non-conforming': 2}
+gender_age1stcode = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-def preprocess(path: str, values: list):
+
+
+def preprocess(path: str, values: list, gender: list):
     # Rows from CSV file to skip
     skip = []
 
@@ -69,13 +73,22 @@ def preprocess(path: str, values: list):
             if row[47] != 'NA' and row[7] != 'NA':
                 if row[0] != 'ResponseId': # Skip the first row
                     values[age_dict[row[7]]].append(int(row[47]))
-        #print(values)
+
+    with open(path, 'r') as csvfile:
+        dr = csv.reader(csvfile)
+        for row in dr:
+            if (row[39] == 'Man' or row[39] == 'Woman' or row[39] == 'Non-binary, genderqueer, or gender non-conforming') and row[7] != 'NA':
+                if row[0] != 'ResponseId': # Skip the first row
+                    gender[age_dict[row[7]]][gender_dict[row[39]]] += 1
+                    
+        print(gender)
         #NBone_att(dr, agefirstcoded)
 
         df = pd.read_csv(path, skiprows = skip)
         return df
 
 def stats_calc(l: list) -> list:
+    # Returns list containing mean, stdev, and variance from list
     res = []
     res.append(st.mean(l))
     res.append(st.stdev(l))
@@ -89,7 +102,14 @@ def get_nb_stats(l) -> list:
     return res
 
 def calc_nb(x: float, mean: float, stdev: float, var: float) -> float:
+    # Naive Bayes probabilistic density formula
     return (1 / (mth.sqrt(2 * mth.pi) * stdev) ) * mth.exp(-((x - mean) ** 2) / (2 * var))
+
+def nb(x: float, stats) -> list:
+    res = []
+    for l in stats:
+        res.append(calc_nb(x, l[0], l[1], l[2]))
+    return res
 
 def NBone_att(dr, values: list):
     print(dr)
@@ -103,9 +123,9 @@ def NBone_att(dr, values: list):
 
 
 if __name__ == '__main__':
-    df = preprocess(FILE_PATH, agefirstcoded)
+    df = preprocess(FILE_PATH, salary_age1stcode, gender_age1stcode)
     #print(df.head())
     #print(df.describe())
-
-    print(get_nb_stats(agefirstcoded))
+    #print(calc_nb(75, 73.28, 5.4989, 30.238))
+    #print(nb(500000, get_nb_stats(agefirstcoded)))
 
